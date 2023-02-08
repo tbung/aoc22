31c72b7afcb1d11e6fb7d0aa034a495f72e29f5a
Till Bungert <tillbungert@gmail.com>
2023-02-09 00:01:31 +0100

    feat(day17): part1

--- day17/main.c
+++ day17/main.c
@@ -1,84 +1,120 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
+#define ARRAY_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
+#define N_ROCKS 2022
+#define ROW 8UL
 
-const uint64_t pieces[5] = {
-   (uint64_t)0b00011110,
+typedef uint32_t piece_t;
+typedef uint8_t row_t;
 
-  ((uint64_t)0b00001000) << 16 |
-  ((uint64_t)0b00011100) << 8 |
-  ((uint64_t)0b00001000),
+const piece_t pieces[] = {
+    (piece_t)0b00011110,
 
-  ((uint64_t)0b00000100) << 16 |
-  ((uint64_t)0b00000100) << 8 |
-  ((uint64_t)0b00011100),
+    ((piece_t)0b00001000) << 16 | ((piece_t)0b00011100) << 8 |
+        ((piece_t)0b00001000),
 
-  ((uint64_t)0b00010000) << 24 |
-  ((uint64_t)0b00010000) << 16 |
-  ((uint64_t)0b00010000) << 8 |
-  ((uint64_t)0b00010000),
+    ((piece_t)0b00000100) << 16 | ((piece_t)0b00000100) << 8 |
+        ((piece_t)0b00011100),
 
-  ((uint64_t)0b00011000) << 8 |
-  ((uint64_t)0b00011000),
+    ((piece_t)0b00010000) << 24 | ((piece_t)0b00010000) << 16 |
+        ((piece_t)0b00010000) << 8 | ((piece_t)0b00010000),
+
+    ((piece_t)0b00011000) << 8 | ((piece_t)0b00011000),
 };
 
-const uint64_t edges = 
-  ((uint64_t)0b01000001) << 56 |
-  ((uint64_t)0b01000001) << 48 |
-  ((uint64_t)0b01000001) << 40 |
-  ((uint64_t)0b01000001) << 32 |
-  ((uint64_t)0b01000001) << 24 |
-  ((uint64_t)0b01000001) << 16 |
-  ((uint64_t)0b01000001) << 8 |
-  ((uint64_t)0b01000001);
+const piece_t right_edge = ((piece_t)0b00000001) << 24 |
+                           ((piece_t)0b00000001) << 16 |
+                           ((piece_t)0b00000001) << 8 | ((piece_t)0b00000001);
 
-void print_piece(uint64_t piece) {
-  char s[64 + 8 + 1] = {0};
+const piece_t left_edge = ((piece_t)0b01000000) << 24 |
+                          ((piece_t)0b01000000) << 16 |
+                          ((piece_t)0b01000000) << 8 | ((piece_t)0b01000000);
 
-  for (int i = 0; i < 8; i++) {
-    int j = 0;
+const piece_t edges = right_edge | left_edge;
+
+void print_piece(piece_t piece) {
+  for (int i = 0; i < 4; i++) {
     uint8_t mask = (uint8_t)1 << ((sizeof(uint8_t) << 3) - 1);
-    while(mask) {
-      s[i * 9 + j] = edges & ((uint64_t)mask << (64 - 8 * (i + 1))) ? 'o' : '.';
+    while (mask) {
+      printf("%c", piece & mask ? '#' : '.');
       mask >>= 1;
-      j++;
     }
-    s[i * 9 + j] = '\n';
-  }
-
-  for (int i = 0; i < 8; i++) {
-    int j = 0;
-    uint8_t mask = (uint8_t)1 << ((sizeof(uint8_t) << 3) - 1);
-    while(mask) {
-      if (piece & ((uint64_t)mask << (64 - 8 * (i + 1)))) {
-        s[i * 9 + j] = '@';
-      }
-      mask >>= 1;
-      j++;
-    }
-  }
-
-  printf("%s", s);
-}
-
-void move_right(uint64_t *piece) {
-  if (((*piece) & edges) == 0) {
-    *piece >>= 1;
+    printf("\n");
   }
 }
 
-void move_left(uint64_t *piece) {
-  if (((*piece) & edges) == 0) {
-    *piece <<= 1;
+void piece_move_right(uint32_t *piece, uint32_t mask) {
+  if ((((*piece) & right_edge) == 0) && ((((*piece) >> 1) & mask) == 0)) {
+    // printf("yes right\n");
+    (*piece) >>= 1;
+  }
+}
+
+void piece_move_left(uint32_t *piece, uint32_t mask) {
+  if ((((*piece) & left_edge) == 0) && ((((*piece) << 1) & mask) == 0)) {
+    // printf("yes left\n");
+    (*piece) <<= 1;
   }
 }
 
 typedef struct {
+  uint8_t *rows;
+  size_t size;
+  size_t capacity;
 } cave_t;
+
+cave_t *cave_new() {
+  cave_t *cave = calloc(1, sizeof(cave_t));
+  cave->size = 0;
+  cave->capacity = 2;
+  cave->rows = calloc(cave->capacity, sizeof(uint8_t));
+
+  return cave;
+}
+
+void cave_push(cave_t *cave, uint8_t row) {
+
+  if (cave->size == cave->capacity) {
+    cave->capacity *= 2;
+    cave->rows = reallocarray(cave->rows, cave->capacity, sizeof(uint8_t));
+    memset(cave->rows + cave->size, 0, cave->size);
+  }
+
+  cave->rows[cave->size] = row;
+  cave->size++;
+}
+
+uint32_t cave_mask(cave_t *cave, size_t height) {
+  if (height >= cave->size) {
+    return 0;
+  }
+  uint32_t result = 0;
+  for (int i = 0; i < 4; i++) {
+    result <<= ROW;
+    if (height + 3 - i >= cave->size) {
+      continue;
+    }
+    result |= cave->rows[height + 3 - i];
+  }
+
+  return result;
+}
+
+void cave_print(cave_t *cave) {
+  for (int i = cave->size - 1; i >= 0; i--) {
+    uint8_t mask = (uint8_t)1 << ((sizeof(uint8_t) << 3) - 1);
+    while (mask) {
+      printf("%c", cave->rows[i] & mask ? '#' : '.');
+      mask >>= 1;
+    }
+    printf("\n");
+  }
+}
 
 typedef struct {
   char *jets;
@@ -106,6 +142,55 @@ char jets_next(jets_t *jets) {
   return result;
 }
 
+void piece_move(uint32_t *piece, uint32_t mask, jets_t *jets) {
+  switch (jets_next(jets)) {
+  case '<':
+    // printf("left\n");
+    piece_move_left(piece, mask);
+    break;
+  case '>':
+    // printf("right\n");
+    piece_move_right(piece, mask);
+    break;
+  default:
+    exit(EXIT_FAILURE);
+  }
+}
+
+void cave_drop(cave_t *cave, jets_t *jets, uint32_t piece) {
+  size_t height = cave->size + 3;
+
+  while (true) {
+    // print_piece(piece);
+    // printf("\n");
+    // cave_print(cave);
+    // printf("\n");
+    // printf("\n");
+    uint32_t current_mask = cave_mask(cave, height);
+    piece_move(&piece, current_mask, jets);
+
+    if (height > cave->size) {
+      height--;
+    } else if ((height == 0) || ((piece & cave_mask(cave, height - 1)) != 0)) {
+      for (int i = 0; i < 4; i++) {
+        uint8_t byte = (uint8_t)(piece >> (ROW * i));
+        if (byte == 0) {
+          break;
+        }
+        if (height < cave->size) {
+          cave->rows[height] |= byte;
+        } else {
+          cave_push(cave, byte);
+        }
+        height++;
+      }
+      return;
+    } else {
+      height--;
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
   FILE *fp;
   char *line = NULL;
@@ -127,7 +212,7 @@ int main(int argc, char *argv[]) {
   jets->size--;
   jets->jets[jets->size] = 0;
 
-  printf("%s\n", jets->jets);
+  // printf("%s\n", jets->jets);
 
   fclose(fp);
 
@@ -135,18 +220,12 @@ int main(int argc, char *argv[]) {
     free(line);
   }
 
-  // for (int i_rock = 0; i_rock < 1; i_rock++) {
-  //   bool stopped = false;
-  // }
-  
-  uint64_t piece = pieces[1];
-  print_piece(piece);
-  printf("\n");
-  for (int i = 0; i < 4; i++) {
-    move_left(&piece);
-    print_piece(piece);
-    printf("\n");
+  cave_t *cave = cave_new();
+  for (int i = 0; i < N_ROCKS; i++) {
+    cave_drop(cave, jets, pieces[i % ARRAY_SIZE(pieces)]);
   }
+  // cave_print(cave);
+  printf("%ld\n", cave->size);
 
   jets_free(jets);
 
